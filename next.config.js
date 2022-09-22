cf5fa23b55191bd3f45f87e54a8ea4863a0af65f
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2022-09-22 21:04:02 +0530

    add: deployment config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  poweredByHeader: false,
+  trailingSlash: false,
+  reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_SITE_PREFIX,
+  assetPrefix: process.env.NEXT_PUBLIC_SITE_PREFIX,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
