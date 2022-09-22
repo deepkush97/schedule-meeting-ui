ecb4954bafb9aa6ac74665e717af1e2a492ea557
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2022-09-22 22:54:29 +0530

    add: functionality of the ui

--- src/components/RightSection.tsx
+++ src/components/RightSection.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useToast } from "../hooks/useToast";
 import { Button } from "./Button";
 import { Input } from "./Input";
 import { TextArea } from "./TextArea";
 
 export const RightSection = () => {
+  const { pushError, pushSuccess } = useToast();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [additionalInfo, setAdditionalInfo] = useState("");
+
+  const handleScheduleEvent = () => {
+    if (!name.trim()) {
+      pushError("Please enter name");
+    } else if (
+      !email.trim() ||
+      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email) === false
+    ) {
+      pushError("Please enter email");
+    } else {
+      pushSuccess(
+        `Name: ${name},
+        Email: ${email} ${
+          additionalInfo &&
+          `, 
+        Addditional Info: ${additionalInfo}       
+        `
+        } `
+      );
+    }
+  };
+
   return (
     <section className="w-full max-w-lg p-6 text-gray-900 mb-2">
       <h5 className=" text-xl mb-2 font-bold">Enter Details</h5>
@@ -11,6 +38,8 @@ export const RightSection = () => {
         controlId="name"
         type="text"
         required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
         label="Name"
         containerClass="mb-4"
       />
@@ -19,6 +48,8 @@ export const RightSection = () => {
         required
         type="email"
         label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         containerClass="mb-2"
       />
       <Button className="mb-4">Add Guests</Button>
@@ -26,8 +57,12 @@ export const RightSection = () => {
         label="Please share anything that will help prepare for our meeting."
         controlId="additional-info"
         containerClass="mb-12"
+        value={additionalInfo}
+        onChange={(e) => setAdditionalInfo(e.target.value)}
       />
-      <Button fill>Schedule Event</Button>
+      <Button fill onClick={handleScheduleEvent}>
+        Schedule Event
+      </Button>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+const defaultConfig = require("tailwindcss/stubs/defaultConfig.stub");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ["Poppins"],
+      sans: ["Poppins", ...defaultConfig.theme.fontFamily.sans],
     },
     extend: {},
   },
