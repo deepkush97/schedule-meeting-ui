2af8e4e1948fdda859ed4ded2167c2eb636ec32a
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2022-09-22 13:55:49 +0530

    refactor: move source into src

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <h1 className="text-3xl text-red-300">Hello World</h1>
+    </div>
+  )
+}
+
+export default Home
